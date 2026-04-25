63d3f12785747f198d11db52f85b9079ba9b2143
Rene Ferreira das Neves <36194883+reneferreira@users.noreply.github.com>
2026-04-24 21:42:17 -0400

    Force HTTPS root URL on Vercel

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -21,6 +21,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         if (app()->environment('production') || env('VERCEL')) {
+            URL::forceRootUrl('https://' . request()->getHttpHost());
             URL::forceScheme('https');
         }
     }
